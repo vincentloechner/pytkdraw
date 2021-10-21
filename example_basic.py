89df3e6c9640731fcc393b5c89c82dfa74cdde78
Vincent Loechner <loechner@unistra.fr>
2021-10-21 17:51:42 +0200

    commented the example

--- example_basic.py
+++ example_basic.py
@@ -1,14 +1,20 @@
+"""Example usage of the basic module."""
 import tkdraw.basic as graph
 
+# size of the window (number of pixels)
 HEIGHT = 400
 WIDTH = 600
 
+# open the window
 graph.open(HEIGHT, WIDTH)
 
+# plot some red diagonals in it
 for i in range(HEIGHT):
     for j in range(WIDTH):
         if ((i-j)//50) % 2 == 0:
             graph.plot(i, j, color="red")
+    # refresh the window every line (if you don't, it refreshes in the wait())
     graph.refresh()
 
+# wait for the user to close the window
 graph.wait()
